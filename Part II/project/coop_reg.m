1285a5363003db21fd9ad4207aa75b549201a1f3
FedericoMorro <fedemorro01@gmail.com>
2024-06-20 16:50:59 +0200

    p02: code reorganization

---
+++ Part II/project/coop_reg.m
@@ -0,0 +1,252 @@
+function [x0_sim, y0_sim, xi_sim, yi_sim, t_sim] = ...
+    coop_reg(Adj, g, A_des_eig, x0_ref, local_obs, noise_vec, silent)
+% Cooperative regulation problem
+
+% Adj: network adjacency matrix
+% g: connections to leader node
+% A_des_eig: desired eigenvalues of local controllers
+% x0_ref: set initial state of x0 which will change regulation
+% local_obs: true/false, use local or global observers
+% noise_vec: vec[0/1], not noise or noise on i-th output
+% silent: 0 plot and print, 1 print, 2 none
+
+
+%% Agents LTI parameters
+A = [
+    0       1
+    880.87  0
+];
+B = [0 -9.9453]';
+C = [708.27 0];
+D = 0;
+
+n = 2;
+m = 1;
+p = 1;
+
+% stability analysis
+if silent < 2
+    eig_A = eig(A)'
+end
+
+
+
+%% CPS paramaters
+N = 6;
+
+% in-degree
+in_deg = zeros(1,N);
+for i = 1:N
+    in_deg(i) = nnz(Adj(i,:)) + nnz(g(i));
+end
+
+% 0 if no noise, 1 if noise
+noise_mean = 0;     noise_var = 10;   noise_seed = randi(100);
+leader_noise = 0;
+
+
+
+%% Leader Controller
+% static state feedback
+%   u = -K*x
+%   x' = (A - B*K)*x
+
+% define static-state feedback loop gain
+if A_des_eig(1) ~= A_des_eig(2)
+    K_contr = place(A,B, A_des_eig);
+else
+    K_contr = acker(A,B, A_des_eig);
+end
+A_contr = A - B*K_contr;
+
+if silent < 2
+    eig_Acontr = eig(A_contr)'
+end
+
+% reference signal gain for x1
+ss_ref = 1;
+
+s = tf('s');
+sys = minreal(zpk(inv(s*eye(n)-A_contr)));
+ss_gain = dcgain(minreal(zpk(s*sys)));
+
+% initial conditions
+if ss_gain(1,1) == 0; x0_0 = 1; else; x0_0 = ss_ref/ss_gain(1,1); end
+x0_0 = x0_0 * x0_ref;
+xi_0 = [0 0];
+
+% subsitute A with controlled versions
+A0 = A;
+A = A_contr;
+
+% impulse response (modal analysis)
+if silent < 1
+    figure
+    impulse(ss(A,B,C,D)), grid on
+end
+
+
+
+%% Local Controllers
+% solve ARE: A'P + PA + Q - PB inv(R) B'P = 0
+R = ones(m,m);
+Q = eye(n);
+P = are(A, B*inv(R)*B', Q);
+
+% compute gain K
+K = inv(R)*B'*P;
+
+% compute coupling gain c
+in_deg_diag = diag(in_deg);         % in-degrees matrix
+L = in_deg_diag - Adj;              % Laplacian matrix of graph
+G = diag(g);            % pinning matrix
+lambda = eig(L+G);       
+c_min = 1 / (2*min(real(lambda)));
+c = 2*c_min;
+
+
+
+%% Global Control Stability
+eig_Ac = [];
+for i = 1:N
+    eig_Ac = [eig_Ac, eig(A - c*lambda(i)*B*K)'];
+end
+if silent < 2
+    eig_Ac
+end
+
+
+
+%% Leader Observer
+% standard Luenberger observer
+L_obs = place(A',C', [-1 -2])';
+x0_0_obs = zeros(n,1);
+
+if silent < 2
+    eig_Lobs = eig(A-L_obs*C)'
+end
+
+
+%% Local Observers
+% observer dynamics
+x_hat = sym('x_hat', [n,1], 'real');
+ct = sym('ct', [n,1], 'real');          % corrective term
+ui = sym('ui', 'real');
+
+x_hat_d = A*x_hat + B*ui - ct;
+
+matlabFunction(x_hat_d, 'File', 'coopObserver', 'Vars', {[x_hat;ui;ct]});
+% used in interpred matlab function in simulink 
+
+% inital condition
+xi_0_obs = zeros(n,1);
+
+% observer gain
+% solve ARE: AP + PA' + Q - PC' inv(R) CP = 0
+Ro = ones(m,m);
+Qo = eye(n);
+Po = are(A', C'*inv(Ro)*C, Qo);
+
+% compute gain F
+F = Po*C'*inv(Ro);
+
+% compute coupling gain co
+co = c;
+
+% use or not local observer
+if local_obs
+    F = L_obs / c;      % s.t. c*F = L_obs, standard Lueneberger
+end
+
+
+
+%% Global Obsever Stability
+eig_Ao = [];
+for i = 1:N
+    if ~local_obs
+        eig_Ao = [eig_Ao, eig(A - co*lambda(i)*F*C)'];
+    else
+        eig_Ao = [eig_Ao, eig(A + co*F*C)'];
+    end
+end
+if silent < 2
+    eig_Ao
+end
+
+
+%% Simulation
+t_sim = 10;
+out = sim('coop_reg_SVFB.slx', 'SrcWorkspace', 'current');
+
+
+
+%% Elaborate results
+
+% reshape node states
+xi_sim = {N};
+for foll_n = 1:N
+    xi_sim{foll_n} = out.xi_hat_all(:,(foll_n-1)*n+1:foll_n*n)';
+end
+% reshape node output errors
+y_tilde_hist = {N};
+for foll_n = 1:N
+    y_tilde_hist{foll_n} = out.yi_tilde_all(:,(foll_n-1)*p+1:foll_n*p)';
+end
+% collect node outputs
+yi_sim = {out.y1(1,:)' out.y2(1,:)' out.y3(1,:)' out.y4(1,:)' out.y5(1,:)' out.y6(1,:)'};
+
+% output values
+t_sim = out.tout;
+x0_sim = out.x0_hat;
+y0_sim = out.y0;
+
+% global disagreement error
+delta = zeros(n*N,length(out.tout));
+delta_MS = zeros(length(out.tout));
+for t = 1:length(out.tout)
+    x0_bar = kron(ones(N,1), out.x0_hat(t,:)');
+    delta(:,t) = out.xi_hat_all(t,:)' - x0_bar;
+    delta_MS(t) = 1/N * norm(delta(:,t))^2;
+end
+
+
+
+%% Plot results
+if silent > 0
+    return
+end
+
+% plot leader dynamics
+figure, plot(t_sim, x0_sim), grid on
+title('Leader S_0')
+legend('x_0','v_0')
+
+% plot nodes states and outputs
+for foll_n = 1:N
+
+    figure
+
+    subplot(3,1,1)
+    plot(t_sim, xi_sim{foll_n}(1,:)), hold on
+    plot(t_sim, x0_sim(:,1), '--'), grid on
+    str = sprintf('x_%i', foll_n); legend(str,'x_0')
+    str = sprintf('S_%i - position', foll_n); title(str)
+
+    subplot(3,1,2)
+    plot(t_sim, xi_sim{foll_n}(2,:)), hold on
+    plot(t_sim, x0_sim(:,2), '--'), grid on
+    str = sprintf('v_%i', foll_n); legend(str,'v_0')
+    str = sprintf('S_%i - velocity', foll_n); title(str)
+
+    subplot(3,1,3)
+    plot(t_sim, yi_sim{foll_n}), hold on
+    plot(t_sim, y0_sim, '--'), grid on
+    str = sprintf('y_%i', foll_n); legend(str,'y_0')
+    str = sprintf('S_%i - output', foll_n); title(str)
+
+end
+
+% global disagreement error
+figure
+plot(out.tout, delta_MS), grid on
+title('Mean Square of global disagreement error')
--- Part II/project/main.m
+++ Part II/project/main.m
@@ -5,217 +5,72 @@ clc
 format compact
 
 
-
-%% Agents LTI parameters
-A = [
-    0       1
-    880.87  0
-];
-B = [0 -9.9453]';
-C = [708.27 0];
-D = 0;
-
-n = 2;
-m = 1;
-p = 1;
-
-% stability analysis
-eig_A = eig(A)'
+% silent: 0 plot and print, 1 print, 2 none
+silent = 2;
 
 
-
-%% CPS paramaters
+%% Setup
+% network parameters
 N = 6;
 
 Adj = zeros(N,N);
-Adj(2,1) = 2;
-Adj(3,2) = 3;
-Adj(4,3) = 6;
-Adj(5,4) = 1;
-Adj(6,5) = 4;
+Adj(3,1) = 2;
+Adj(5,1) = 2;
+Adj(4,2) = 2;
+Adj(6,2) = 2;
 
 g = zeros(N,1);
-g(1) = 1;
+g(1) = 2;
+g(2) = 2;
 
+aug_graph = [
+    zeros(1,N+1)
+    g       Adj
+];
+digr = digraph(aug_graph', {'0','1','2','3','4','5','6'});
 
-
-%% Leader Controller
-% static state feedback
-%   u = -K*x
-%   x' = (A - B*K)*x
-
-% define static-state feedback loop gain
-%K_contr = place(A,B, 0.5*[0 -1]);           % step
-%K_contr = acker(A,B, 0.5*[0 0]);            % ramp
-K_contr = place(A,B, 2*[0+1i 0-1i]);        % sinusoidal
-A_contr = A - B*K_contr;
-
-eig_Acontr = eig(A_contr)'
-
-% reference signal gain for x1
-ss_ref = 1;
-
-s = tf('s');
-sys = minreal(zpk(inv(s*eye(n)-A_contr)));
-ss_gain = dcgain(minreal(zpk(s*sys)));
-
-% initial conditions
-if ss_gain(1,1) == 0; x0_0 = 1; else; x0_0 = ss_ref/ss_gain(1,1); end
-x0_0 = x0_0 * [1 0];
-xi_0 = zeros(n,1);
-
-% subsitute A with controlled versions
-A0 = A;
-A = A_contr;
-
-% impulse response (modal analysis)
-impulse(ss(A,B,C,D)), grid on
-
-
-
-%% Local Controllers
-% solve ARE: A'P + PA + Q - PB inv(R) B'P = 0
-R = ones(m,m);
-Q = eye(n);
-P = are(A, B*inv(R)*B', Q);
-
-% compute gain K
-K = inv(R)*B'*P;
-
-% compute coupling gain c
-in_deg = diag(ones(6,1));    % in-degrees
-L = in_deg - Adj;            % Laplacian matrix of graph
-G = diag(g);            % pinning matrix
-lambda = eig(L+G);       
-c_min = 1 / (2*min(real(lambda)));
-c = 2*c_min;
-
-
-
-%% Global Control Stability
-eig_Ac = [];
-for i = 1:N
-    eig_Ac = [eig_Ac, eig(A - c*lambda(i)*B*K)'];
+if 1
+    figure, plot(digr, 'EdgeLabel', digr.Edges.Weight)
 end
-eig_Ac
 
+% desired eigenvalues
+A_des_eig = 0.5*[0 -1];         % constant
+%A_des_eig = [0 0];              % ramp          
+%A_des_eig = 2*[0+1i 0-1i];      % sinusoidal
 
+% reference to be tracked
+x0_ref = [1 0];
 
-%% Leader Observer
-% standard Luenberger observer
-L_obs = place(A',C', [-1 -2])';
-x0_0_obs = zeros(n,1);
+% noise
+noise_vec = zeros(N,1);
 
-eig_Lobs = eig(A-L_obs*C)'
-
-
-
-%% Local Observers
-% observer dynamics
-x_hat = sym('x_hat', [n,1], 'real');
-ct = sym('ct', [n,1], 'real');          % corrective term
-ui = sym('ui', 'real');
-
-x_hat_d = A*x_hat + B*ui - ct;
-
-matlabFunction(x_hat_d, 'File', 'coopObserver', 'Vars', {[x_hat;ui;ct]});
-% used in interpred matlab function in simulink 
-
-% inital condition
-xi_0_obs = zeros(n,1);
-
-% observer gain
-% solve ARE: AP + PA' + Q - PC' inv(R) CP = 0
-Ro = ones(m,m);
-Qo = eye(n);
-Po = are(A', C'*inv(Ro)*C, Qo);
-
-% compute gain F
-F = Po*C'*inv(Ro);
-
-% compute coupling gain co
-co = c;
-
-% use or not local observer
+% local or global observers
 local_obs = false;
-if local_obs
-    F = L_obs / c;      % s.t. c*F = L_obs, standard Lueneberger
-end
 
 
 
-%% Global Obsever Stability
-eig_Ao = [];
+%% Simulation(s)
+[x0_sim, y0_sim, xi_sim, yi_sim, t_sim] = ...
+    coop_reg(Adj, g, A_des_eig, x0_ref, local_obs, noise_vec, silent);
+
+
+
+%% Output elaboration
+% followers step response
+rise_set_time = zeros(N,2);
 for i = 1:N
-    if ~local_obs
-        eig_Ao = [eig_Ao, eig(A - co*lambda(i)*F*C)'];
-    else
-        eig_Ao = [eig_Ao, eig(A + co*F*C)'];
-    end
+    s = stepinfo(yi_sim{i}, t_sim);
+    rise_set_time(i,:) = [s.RiseTime, s.SettlingTime];
 end
-eig_Ao
-
-
-
-%% Simulation
-t_sim = 10;
-out = sim('coop_reg_SVFB.slx');
-
-
-
-%% Plot results
-close all
-
-% plot leader dynamics
-figure, plot(out.tout, out.x0_hat), grid on
-title('Leader S_0')
-legend('x_0','v_0')
-
-% reshape node states
-x_hist = zeros(n,length(out.tout),N);
-for foll_n = 1:N
-    x_hist(:,:,foll_n) = out.xi_hat_all(:,(foll_n-1)*n+1:foll_n*n)';
-end
-% reshape node output errors
-y_hist = zeros(p,length(out.tout),N);
-for foll_n = 1:N
-    y_hist(:,:,foll_n) = out.yi_tilde_all(:,(foll_n-1)*p+1:foll_n*p)';
-end
-
-% plot nodes states and outputs
-for foll_n = 1:N
-
-    figure
-
-    subplot(3,1,1)
-    plot(out.tout, x_hist(1,:,foll_n)), hold on
-    plot(out.tout, out.x0_hat(:,1), '--'), grid on
-    str = sprintf('x_%i', foll_n); legend(str,'x_0')
-    str = sprintf('S_%i - position', foll_n); title(str)
-
-    subplot(3,1,2)
-    plot(out.tout, x_hist(2,:,foll_n)), hold on
-    plot(out.tout, out.x0_hat(:,2), '--'), grid on
-    str = sprintf('v_%i', foll_n); legend(str,'v_0')
-    str = sprintf('S_%i - velocity', foll_n); title(str)
-
-    subplot(3,1,3)
-    plot(out.tout, y_hist(1,:,foll_n)), hold on
-    plot(out.tout, out.y0_tilde, '--'), grid on
-    str = sprintf('err_{y_%i}', foll_n); legend(str,'err_{y_0}')
-    str = sprintf('S_%i - output estimation error', foll_n); title(str)
-
-end
-
-% compute and plot global disagreement error
-delta = zeros(n*N,length(out.tout));
-delta_MS = zeros(length(out.tout));
-for t = 1:length(out.tout)
-    x0_bar = kron(ones(N,1), out.x0_hat(t,:)');
-    delta(:,t) = out.xi_hat_all(t,:)' - x0_bar;
-    delta_MS(t) = 1/N * norm(delta(:,t))^2;
-end
-
 figure
-plot(out.tout, delta_MS), grid on
-title('Mean Square of global disagreement error')
+plot(1:N, rise_set_time, 'o'), grid on
+legend('Rise time', 'Settling time')
+xlabel('Follower #'), ylabel('seconds')
+
+% step responses plot
+figure, grid on, hold on
+plot(t_sim,y0_sim,'k--', 'LineWidth',1.2, 'DisplayName','S_0')
+for i = 1:N
+    plot(t_sim,yi_sim{i}, 'DisplayName',sprintf("S_%i", i))
+end
+title('Followers step response'), legend
